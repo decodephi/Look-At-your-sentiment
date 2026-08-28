169b6a04d7e19d6736f107e190f284c0ef74a056
Pranab Samanta <pranabsamanta381@gmail.com>
2026-08-28 22:56:26 +0530

    allow cros

--- app/main.py
+++ app/main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routes import router
 
-
-# ============================================================
-# FastAPI Application
-# ============================================================
 
 app = FastAPI(
     title="Sentiment MLOps API",
@@ -15,9 +12,22 @@ app = FastAPI(
 
 
 # ============================================================
+# CORS
+# ============================================================
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# ============================================================
 # Routes
 # ============================================================
 
-app.include_router(
-    router
-)
+app.include_router(router)
